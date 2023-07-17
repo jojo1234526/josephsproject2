d2cd93a061021f705b7bf6b17b2559a00a520ab5
jojo1234526 <121139382+jojo1234526@users.noreply.github.com>
2023-07-17 13:40:38 -0500

    Initial commit

---
+++ src/main/java/revatureAPIcreation/josephsproject/JosephsprojectApplication.java
@@ -0,0 +1,13 @@
+package revatureAPIcreation.josephsproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JosephsprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JosephsprojectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/revatureAPIcreation/josephsproject/JosephsprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package revatureAPIcreation.josephsproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JosephsprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
